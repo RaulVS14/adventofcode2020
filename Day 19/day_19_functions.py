ec01c088ad4b8d09a75522575971a2bb02cfe28c
Raul <raul.muser@gmail.com>
2020-12-21 17:40:03 +0200

    Cleanup setup for day 19

--- Day 19/day_19_functions.py
+++ Day 19/day_19_functions.py
@@ -3,15 +3,21 @@ import re
 from helpers.helpers import read_file
 
 
+def find_suitable_messages_for_rule_0(rules):
+    rule_0 = rules["0"]
+    return False
+
+
 def find_total_of_messages_that_match_rule_0(file_name):
     file = read_file(file_name)
     rules, messages = process_file(file)
+    potential_messages = find_suitable_messages_for_rule_0(rules)
     return file
 
 
 def process_file(file):
     rules ={}
-    message =[]
+    messages =[]
     for line in file:
         regex_line = re.match(r'^(?P<key>\d*)\: "?(?P<value>[a-z0-9\| ]*)"?$', line)
         if regex_line:
@@ -21,8 +27,8 @@ def process_file(file):
         elif line == "":
             continue
         else:
-            message.append(line)
-    return rules, message
+            messages.append(line)
+    return rules, messages
 
 
 def section_2_function(file_name):
